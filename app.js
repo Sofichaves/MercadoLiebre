b41659ccfbf76c0ae57b398b3ab63cbafccd38de
Sofia Chaves <sofiamagdalenachaves@gmail.com>
2022-10-26 21:43:23 -0300

    puerto config

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ const publicPath = path.resolve(__dirname, './public');
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-console.log("Servidor funcionando");
-});
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log("Servidor funcionando"));
 
 app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname, './views/home.html'));
